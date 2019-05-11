1a1156e81965590883182279d74f1276b15e20fd
fangweiren <fwr843@163.com>
2019-05-11 23:26:34 +0800

    QQ音乐播放工具类封装、播放暂停

--- code/65-QQ音乐播放器/js/index.js
+++ code/65-QQ音乐播放器/js/index.js
@@ -1,58 +1,10 @@
 $(function () {
-    // 3.自定义滚动条
+    // 0.自定义滚动条
     $(".content_list").mCustomScrollbar();
 
-    // 1.监听歌曲的移入移出事件
-    $(".content_list").delegate(".list_music", "mouseenter", function () {
-        // 显示子菜单
-        $(this).find(".list_menu").stop().fadeIn(100);
-        $(this).find(".list_time a").stop().fadeIn(100);
-        // 隐藏时长
-        $(this).find(".list_time span").stop().fadeOut(100);
-    });
-    $(".content_list").delegate(".list_music", "mouseleave", function () {
-        // 隐藏子菜单
-        $(this).find(".list_menu").stop().fadeOut(100);
-        $(this).find(".list_time a").stop().fadeOut(100);
-        // 显示时长
-        $(this).find(".list_time span").stop().fadeIn(100);
-    });
-
-    // 2.监听复选框的点击事件
-    $(".content_list").delegate(".list_check", "click", function () {
-        $(this).toggleClass("list_checked");
-    });
-
-    // 5.子菜单播放按钮的监听
-    var $musicPlay = $(".music_play");
-    $(".content_list").delegate(".list_menu_play", "click", function () {
-        var $item = $(this).parents(".list_music");
-        // 5.1 切换播放图标
-        $(this).toggleClass("list_menu_play2");
-        // 5.2 复原其它的播放图标
-        $item.siblings().find(".list_menu_play").removeClass("list_menu_play2");
-        // 5.3 同步底部播放按钮
-        if ($(this).attr("class").indexOf("list_menu_play2") != -1) {
-            // 当前子菜单播放按钮是播放状态
-            $musicPlay.addClass("music_play2");
-            // 让文字高亮
-            $item.find("div").css("color", "#fff");
-            // 排他思想
-            $item.siblings().find("div").css("color", "rgba(255, 255, 255, 0.5)");
-
-        } else {
-            // 当前子菜单播放按钮是暂停状态
-            $musicPlay.removeClass("music_play2");
-            // 让文字不高亮
-            $item.find("div").css("color", "rgba(255, 255, 255, 0.5)");
-        }
-
-        // 5.4 切换序号的状态
-        $item.find(".list_number").toggleClass("list_number2");
-        $item.siblings().find(".list_number").removeClass("list_number2");
-    });
-
-    // 4.加载歌曲列表
+    var $audio = $("audio");
+    var player = new Player($audio);
+    // 1.加载歌曲列表
     getPlayList();
 
     function getPlayList() {
@@ -60,6 +12,7 @@ $(function () {
             url: "./source/musiclist.json",
             dataType: "json",
             success: function (data) {
+                player.musicList = data;
                 // 4.1 遍历获取到的数据，创建每一条数据
                 var $musicList = $(".content_list ul");
                 $.each(data, function (index, ele) {
@@ -72,6 +25,66 @@ $(function () {
             }
         });
     }
+
+
+    // 2.初始化事件监听
+    initEvents();
+
+    function initEvents() {
+        // 1.监听歌曲的移入移出事件
+        $(".content_list").delegate(".list_music", "mouseenter", function () {
+            // 显示子菜单
+            $(this).find(".list_menu").stop().fadeIn(100);
+            $(this).find(".list_time a").stop().fadeIn(100);
+            // 隐藏时长
+            $(this).find(".list_time span").stop().fadeOut(100);
+        });
+        $(".content_list").delegate(".list_music", "mouseleave", function () {
+            // 隐藏子菜单
+            $(this).find(".list_menu").stop().fadeOut(100);
+            $(this).find(".list_time a").stop().fadeOut(100);
+            // 显示时长
+            $(this).find(".list_time span").stop().fadeIn(100);
+        });
+
+        // 2.监听复选框的点击事件
+        $(".content_list").delegate(".list_check", "click", function () {
+            $(this).toggleClass("list_checked");
+        });
+
+        // 5.子菜单播放按钮的监听
+        var $musicPlay = $(".music_play");
+        $(".content_list").delegate(".list_menu_play", "click", function () {
+            var $item = $(this).parents(".list_music");
+            // 5.1 切换播放图标
+            $(this).toggleClass("list_menu_play2");
+            // 5.2 复原其它的播放图标
+            $item.siblings().find(".list_menu_play").removeClass("list_menu_play2");
+            // 5.3 同步底部播放按钮
+            if ($(this).attr("class").indexOf("list_menu_play2") != -1) {
+                // 当前子菜单播放按钮是播放状态
+                $musicPlay.addClass("music_play2");
+                // 让文字高亮
+                $item.find("div").css("color", "#fff");
+                // 排他思想
+                $item.siblings().find("div").css("color", "rgba(255, 255, 255, 0.5)");
+
+            } else {
+                // 当前子菜单播放按钮是暂停状态
+                $musicPlay.removeClass("music_play2");
+                // 让文字不高亮
+                $item.find("div").css("color", "rgba(255, 255, 255, 0.5)");
+            }
+
+            // 5.4 切换序号的状态
+            $item.find(".list_number").toggleClass("list_number2");
+            $item.siblings().find(".list_number").removeClass("list_number2");
+
+            // 5.5 播放
+            player.playMusic($item.get(0).index, $item.get(0).music);
+        });
+    }
+
 
     /**
      * 创建一条音乐
@@ -97,6 +110,8 @@ $(function () {
             "                        </div>\n" +
             "                    </li>");
 
+        $item.get(0).index = index;
+        $item.get(0).music = music;
         return $item;
     }
 });
---
+++ code/65-QQ音乐播放器/js/player.js
@@ -0,0 +1,33 @@
+(function (window) {
+    function Player($audio) {
+        return new Player.prototype.init($audio);
+    }
+
+    Player.prototype = {
+        constructor: Player,
+        musicList: [],
+        init: function ($audio) {
+            this.$audio = $audio;
+            this.audio = $audio.get(0);
+        },
+        currentIndex: -1,
+        playMusic: function (index, music) {
+            // 判断是否是同一首音乐
+            if (this.currentIndex == index) {
+                // 同一首音乐
+                if (this.audio.paused) { // 如果是暂停
+                    this.audio.play(); // 播放
+                } else {
+                    this.audio.pause();
+                }
+            } else {
+                // 不是同一首
+                this.$audio.attr("src", music.link_url);
+                this.audio.play();
+                this.currentIndex = index;
+            }
+        }
+    };
+    Player.prototype.init.prototype = Player.prototype;
+    window.Player = Player;
+})(window);
